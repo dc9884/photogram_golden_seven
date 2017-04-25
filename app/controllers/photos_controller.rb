fe0edc34e96728f910783273105b8873556d17bb
David Caine <dc9884@users.noreply.github.com>
2017-04-25 11:31:50 -0500

    functional create, read, update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,16 +7,52 @@ def index
   render("photos/index.html.erb")
 end
 
+def new_form
+
+  render("photos/new_form.html.erb")
+end
+
+def create_row
+
+  p = Photo.new
+  p.source = params["the_image_url"]
+  p.caption = params["the_caption"]
+  p.save
+  redirect_to("/photos")
+end
+
 def show
 
-  id_number = params["photo_id"]
-  p = Photo.find(id_number)
+  @photo =  Photo.find(params[:photo_id])
 
-  @image_url = p.source
-  @caption = p.caption
-  @created = p.created_at
+  @photo_id = @photo.id
+  @image_url = @photo.source
+  @caption = @photo.caption
+  @created = @photo.created_at
 
   render("photos/show.html.erb")
 end
+
+def edit_form
+
+  @photo = Photo.find(params[:photo_id])
+  @photo_id = @photo.id
+  @photo_source = @photo.source
+  @photo_caption = @photo.caption
+
+  render("photos/edit_form.html.erb")
+
+end
+
+def update_row
+
+  p = Photo.find(params[:photo_id])
+  p.source = params[:the_source]
+  p.caption = params[:the_caption]
+  p.save
+
+  redirect_to("/photos")
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
 
+# CREATE
+get("/photos/new", { :controller => "photos", :action => "new_form" })
+
+get("/create_photo", { :controller => "photos", :action => "create_row" })
+
+#READ
+get("/photos", { :controller => "photos", :action => "index" })
+
 get("/photos/:photo_id", { :controller => "photos", :action => "show" })
 
-get("/photos", { :controller => "photos", :action => "index" })
+#UPDATE
+get("/photos/:photo_id/edit", { :controller => "photos", :action => "edit_form" })
+
+get("/update_photo/:photo_id", { :controller => "photos", :action => "update_row" })
+
+#DELETE
+#DELETE GOES HERE
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
